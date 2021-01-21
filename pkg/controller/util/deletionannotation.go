79ef3b245fd219fe2bd6701150d089f8fcde6513
Roland.Ma <rolandma@yunify.com>
2021-01-21 05:20:26 +0000

    feat: delete resource with DeleteOptioins

---
+++ pkg/controller/util/deletionannotation.go
@@ -0,0 +1,48 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"encoding/json"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	// DeleteOptionAnnotation contains options for delete
+	// while deleting resources for member clusters.
+	DeleteOptionAnnotation = "kubefed.io/deleteoption"
+)
+
+// GetDeleteOptions return delete options from the annotation
+func GetDeleteOptions(obj *unstructured.Unstructured) []client.DeleteOption {
+
+	options := make([]client.DeleteOption, 0)
+	annotations := obj.GetAnnotations()
+	if annotations == nil {
+		return options
+	}
+
+	if optStr, ok := annotations[DeleteOptionAnnotation]; ok {
+		opt := &client.DeleteOptions{}
+		if err := json.Unmarshal([]byte(optStr), opt); err == nil {
+			options = append(options, opt)
+		}
+	}
+	return options
+}
---
+++ pkg/controller/util/deletionannotation_test.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func TestDeleteOptions(t *testing.T) {
+
+	fedObj := &unstructured.Unstructured{}
+
+	fedObjOrphan := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"annotations": map[string]interface{}{
+					DeleteOptionAnnotation: "{\"PropagationPolicy\":\"Orphan\"}",
+				},
+			},
+		},
+	}
+
+	fedObjGrace := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"annotations": map[string]interface{}{
+					DeleteOptionAnnotation: "{\"GracePeriodSeconds\":5}",
+				},
+			},
+		},
+	}
+
+	fedObjGraceAndOrphan := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"annotations": map[string]interface{}{
+					DeleteOptionAnnotation: "{\"PropagationPolicy\":\"Orphan\",\"GracePeriodSeconds\":5}",
+				},
+			},
+		},
+	}
+
+	actOpt0 := client.DeleteOptions{}
+	actOpt0.ApplyOptions(GetDeleteOptions(fedObj))
+	expOpt0 := client.DeleteOptions{}
+	assert.Equal(t, expOpt0, actOpt0)
+
+	actOpt1 := client.DeleteOptions{}
+	actOpt1.ApplyOptions(GetDeleteOptions(fedObjOrphan))
+	prop := metav1.DeletePropagationOrphan
+	expOpt1 := client.DeleteOptions{PropagationPolicy: &prop}
+	assert.Equal(t, expOpt1, actOpt1)
+
+	actOpt2 := client.DeleteOptions{}
+	actOpt2.ApplyOptions(GetDeleteOptions(fedObjGrace))
+	seconds := int64(5)
+	expOpt2 := client.DeleteOptions{GracePeriodSeconds: &seconds}
+	assert.Equal(t, expOpt2, actOpt2)
+
+	actOpt3 := client.DeleteOptions{}
+	actOpt3.ApplyOptions(GetDeleteOptions(fedObjGraceAndOrphan))
+	expOpt3 := client.DeleteOptions{GracePeriodSeconds: &seconds, PropagationPolicy: &prop}
+	assert.Equal(t, expOpt3, actOpt3)
+}
